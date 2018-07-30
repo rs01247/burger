b6621c63c49eb2c2f483a5c9de00c7f487c1da76
Raymond <rs01247@gmail.com>
2018-07-30 16:12:40 -0700

    orm issues

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,13 @@
 const conn = require('./connection');
 
+function objToSql(ob) {
+    let arr = [];
+    for (let key in ob) {
+        arr.push(key + "=" + ob[key]);
+    }
+    return arr.toString();
+}
+
 const orm = {
     selectAll: function (tableInput, cb) {
         let queryString = 'SELECT * FROM ??';
@@ -10,14 +18,14 @@ const orm = {
     },
     insertOne: function (tableInput, colInput, userVal, cb) {
         let queryString = 'INSERT INTO ?? (??) VALUES (??)';
-        conn.query(queryString, [tableInput, colInput, userVal], function (err, result) {
+        conn.query(queryString, [tableInput, colInput.toString(), userVal], function (err, result) {
             if (err) throw err;
             cb(result);
         })
     },
     updateOne: function (tableInput, colInput, condition, cb) {
         let queryString = 'UPDATE ?? SET ?? WHERE ??';
-        conn.query(queryString, [tableInput, colInput, condition], function (err, result) {
+        conn.query(queryString, [tableInput, objToSql(colInput), condition], function (err, result) {
             if (err) throw err;
             cb(result);
         })
